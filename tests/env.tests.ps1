78ed085bf4ccbc0edd6b0eb3b3d7fccf933f4275
Sniffer <sm.sniffer@gmail.com>
2020-03-10 00:11:36 +1100

    update pipeline

--- tests/env.tests.ps1
+++ tests/env.tests.ps1
@@ -3,7 +3,7 @@ Set-StrictMode -Version latest
 Describe 'BuildEnvironment' {
 
     Context 'Variables' {
-        It "ENV:VAR should not be null" {
+        It "ENV:VAR's should not be null" {
 
             $ENV:AZURE_LOCATION | Should Not Be $Null
             $ENV:AZURE_ACR_NAME | Should Not Be $Null
