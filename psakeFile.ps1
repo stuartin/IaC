e021be76e465f3610a78a33ce6e7c76835249730
Sniffer <sm.sniffer@gmail.com>
2020-03-10 00:18:31 +1100

    testing rg deployment

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -1,28 +1,28 @@
 task default -depends Test
 
 task Test {
-    Write-Output "------- WRITE TESTS TO EXECUTE AND UPDATE TEST TASK -------"
     Invoke-Pester "$PSScriptRoot\tests"
 }
 
 task Setup -Depends Test {
-    # setup azure cli
+    Write-Output "Setting Azure CLI defaults..."
     $defaults = @(
         "--location", "$ENV:AZURE_LOCATION", # default geo location
         "--acr", "$ENV:AZURE_ACR_NAME" # default acr (azure container registry) name
     )
 
-    #az configure --defaults @defaults
+    az configure --defaults @defaults
+    az configure --list-defaults
 }
 
 task Deploy -Depends Test, Setup {
-
-    # deploy resource group
+    Write-Output "Creating Resource Group..."
     $params = @(
-        "--name", "test", 
+        "--name", "$ENV:AZURE_RG_NAME", 
         "--tags", "test2"
     )
-    #az group create @params
+    az group create @params
+    
     #az appservice plan create --name $(appserviceplan)  --resource-group $(resourceGroupName) --sku FREE
     #az webapp create --name $(apiapp) --resource-group $(resourceGroupName) --plan $(appserviceplan) 
     #az webapp identity assign -g $(resourceGroupName)  -n $(apiapp)
--- tests/env.tests.ps1
+++ tests/env.tests.ps1
@@ -6,6 +6,7 @@ Describe 'BuildEnvironment' {
         It "ENV:VAR's should not be null" {
 
             $ENV:AZURE_LOCATION | Should Not Be $Null
+            $ENV:AZURE_RG_NAME | Should Not Be $Null
             $ENV:AZURE_ACR_NAME | Should Not Be $Null
 
         }
